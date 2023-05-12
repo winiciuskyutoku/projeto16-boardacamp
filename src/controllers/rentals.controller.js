575a35877c1049b037cda7968f0ba1e11d5ffaa8
winiciuskyutoku <winicius123098@gmail.com>
2023-05-12 08:32:10 -0300

    feat: post new rentals feature

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,10 +1,9 @@
 import { db } from "../database/database.connection.js";
+import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
 
     try {
-        console.log(id)
-
         const rentals = await db.query(`
             SELECT rentals.*, customers.id  AS cid, customers.name AS cn,  games.id AS gid, games.name AS gn
             FROM rentals
@@ -26,6 +25,39 @@ export async function getRentals(req, res) {
         })
 
         res.send(newRentals)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function postRentals(req, res) {
+    const { customerId, gameId, daysRented } = req.body
+    const rentDate =  dayjs().format('YYYY-MM-DD')
+    const returnDate = null
+    const delayFee = null
+
+    try {
+        if(daysRented <= 0) return res.status(400).send("Insiria mais que um dia")
+
+        const price = await db.query(`SELECT "games"."pricePerDay" FROM games WHERE id = ${gameId}`)
+
+        const originalPrice = Number(daysRented) * Number(price.rows[0].pricePerDay)
+
+        const checkCustomer = await db.query(`SELECT * FROM customers WHERE id = ${customerId}`)
+        if(!checkCustomer.rows[0]) return res.status(400).send("Esse usuario nao existe")
+
+        const checkGame = await db.query(`SELECT * FROM games WHERE id = ${gameId}`)
+        if(!checkGame.rows[0]) return res.status(400).send("Esse videogame nao existe")
+
+        const checkAvailability = await db.query(`SELECT "games"."stockTotal" FROM games WHERE id = ${gameId}`)
+        if(checkAvailability.rows[0].stockTotal <= 0) return res.status(400).send("Game indisponivel")
+
+        await db.query(`
+            INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+            VALUES (${customerId}, ${gameId}, '${rentDate}', ${daysRented}, ${returnDate}, ${originalPrice}, ${delayFee});
+        `)
+
+        res.send(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getRentals } from "../controllers/rentals.controller.js";
+import { getRentals, postRentals } from "../controllers/rentals.controller.js";
 
 const rentalRouter = Router()
 
 rentalRouter.get("/rentals", getRentals)
+rentalRouter.post("/rentals", postRentals)
 
 export default rentalRouter
